8ad11bc8d26384deeb50747d77ca219253618a24
gordonCharles <gacbuy@gmail.com>
2024-03-06 21:17:20 -0800

    Tested and keys / ids scrubbed

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# CloudflareDDNS
+#A python script for updating a the A records on list of domains on a cloudflare to support DDNS.
+
+`ddns.py` is intended to be run as a service at intervals to check for changes of the IP address of the server the service is running on.  If the IP address changes, the correspoding A records in Cloudflare will be updated.
+
+The only user inputs are:
+
+1) `API_TOKEN` - the Cloudflare API token - Created on Cloudfare web interface under My proifile -> API Tokens -> User API Tokens (versus API Keys)
+
+
+2) Zone IDs - 32 character identfiers - Found in Main Menu -> Websites -> <Specific Website Button> then scroll down the right hand column.
+
+`ddns.py` will check for a cache file, `IP_Address_Cache.pkl`, containing the A record IP addresses currently set on Cloudflare and their corresponding Cloudflare record IDs.  If the file is absent or a new site has been added, `ddns.py` will query Cloudflare for the new information.  Using the cache eliminates unneeded queries.  If a site / recoard needs to be deleted, delete the cache file and remove the corresponding Zone ID from the `zone_ids` dictionary.  The cache will be auto-populated the next time it runs.
+
+If the reported IP address for the server does not match any of the cached values in the A record, the Cloudflare API call to update the DNS record will be performed.
+
+To support automation of the script, I've created a systemd service and corresponding timer.  For details see the following two sections.
+
+##Enabling initialization at boot (auto launch)
+
+Edit values of `ExecStart` and `WorkingDirectory` in `ddns.service` to match your setup.
+
+Copy `ddns.service` into `/etc/systemd/system` as root:
+
+> sudo cp ddns.service /etc/systemd/system/ddns.service
+
+Attempt to start the service using the following command:
+
+> sudo systemctl start ddns.service
+
+Stop it using following command:
+
+> sudo systemctl stop ddns.service
+
+To have it start automatically on reboot by using this command:
+
+> sudo systemctl enable ddns.service
+
+The systemctl command can also be used to restart the service or disable it
+
+
+
+##Enabling a timer to restart the service at regular intervals
+
+Note the file prefix for the timer must match the prefix for the service.  The timer is setup for running every 5 minutes.
+
+Copy `ddns.timer` into `/etc/systemd/system` as root, for example:
+
+> sudo cp ddns.timer /etc/systemd/system/ddns.timer
+
+Once this has been copied, the timer needs to be enabled:
+
+> sudo systemctl enable ddns.timer
+
+Then the timer needs to be started:
+
+> sudo systemctl start ddns.timer
+
+Check status with:
+
+> systemctl status ddns.service
+
+Example status from working system:
+
+> ● ddns.service - DDNS Client for Cloudflare
+>      Loaded: loaded (/etc/systemd/system/ddns.service; disabled; vendor preset: enabled)
+>      Active: inactive (dead) since Thu 2024-03-07 01:05:08 UTC; 48s ago
+> TriggeredBy: ● ddns.timer
+>     Process: 251334 ExecStart=/usr/bin/python3 -u ddns.py --serviceMode (code=exited, status=0/SUCCESS)
+>    Main PID: 251334 (code=exited, status=0/SUCCESS)
+> 
+> Mar 07 01:05:08 odroid python3[251334]: 200
+> Mar 07 01:05:08 odroid python3[251334]: <Response [200]>
+
+##Cloudflare docs:
+
+
+Query DNS records:
+
+[https://developers.cloudflare.com/api/operations/dns-records-for-a-zone-list-dns-records]()
+
+Set DNS records:
+
+[https://developers.cloudflare.com/api/operations/dns-records-for-a-zone-update-dns-record
+]()
+
+Some code and concept lifted from:
+
+[https://github.com/creimers/cloudflare-ddns]()
+
+##Python & Package Revisions:
+
+Python 3.8.10
+
+##Testing:
+
+###Python
+The two Cloudfare interface calls are split and pretty easily isolated for testing; alhtough, they shouls just work.  The curl command in the comments of `ddns.py` is a simple method to test out your zone IDs and API Token.
+
+###systemd service
+Once both the service and timer are running `systemctl status ddns.service` provides commpact but useful feedback on the state of the service including the time since it was last run.  A simple metric to use when bringing up the service is to delete `IP_Address_Cache.pkl` as it will be regenerated if deleted every time the service is run.
+
+###DNS propagation
+It was acceptable and easisted for me to test the propagation by changing the DNS reecord through Cloudfare's web portal to an incorrect value and watch for the creation of `IP_Address_Cache.pkl` or the roll over of the timer from `systemctl status ddns.service` to check to see if the service correctly updated the A record.
+
---
+++ ddns.py
@@ -0,0 +1,183 @@
+'''
+ddns.py is intended to be run as a service at intervals to check for changes of the IP address of the server
+the service is running on.  If the IP address changes, the corresponding A records in Cloudflare will be updated.
+The only user inputs are:
+
+1) API_TOKEN - the Cloudflare API token - Created under My profile -> API Tokens -> User API Tokens (versus API Keys)
+
+2) Zone IDs - 32 character identifiers - Found in Main Menu -> Websites -> <Specific Website Button> then scroll down
+                                        the right hand column.
+
+ddns.py will check for a cache file containing the A record IP addresses currently set on Cloudflare and their corresponding
+Cloudflare record IDs.  If the file is absent or a new site has been added, ddns.py will query Cloudflare for the
+new information.  Using the cache eliminates unneeded queries.  If a site / record needs to be deleted, delete the cache
+file and remove the corresponding Zone ID from the zone_ids dictionary.  The cache will be auto-populated the next time 
+it runs.
+
+If the reported IP address for the server does not match any of the cached values in the A record, the Cloudflare API
+call to update the DNS record will be performed.
+
+
+Cloudflare docs:
+================
+
+Query DNS records:
+
+https://developers.cloudflare.com/api/operations/dns-records-for-a-zone-list-dns-records
+
+Set DNS records:
+
+https://developers.cloudflare.com/api/operations/dns-records-for-a-zone-update-dns-record
+
+Some code and concept lifted from:
+
+https://github.com/creimers/cloudflare-ddns
+'''
+
+import json
+import os
+import requests
+from pprint import pprint
+import pickle
+
+"""
+curl call used to 
+
+1) debug security API 
+2) optionally get record ID in the response.  The record ID is required for changing the record; 
+   however, the Cloudflare web portal does not display the record ID unless it has just been saved.
+   The python implementation, automatically pulls the record IDs based on the the supplied zone IDs
+   and stores the record IDs in a cache.
+
+Note this does not match the curl call supplied in Cloudflare's API example:
+
+https://developers.cloudflare.com/api/operations/dns-records-for-a-zone-list-dns-records
+
+which appears to be incomplete and for a different method of authentication.  This method was taken from:
+
+https://dash.cloudflare.com/profile/api-tokens
+
+
+curl --request GET \
+  --url https://api.cloudflare.com/client/v4/zones/398a6ba0a4d64bd818ec0f0f3ea7186e/dns_records \
+  --header 'Content-Type: application/json' \
+  --header 'Authorization: Bearer WrR-_5offZOlPL6GxssPN5rZiVQziYsr'
+"""
+
+API_TOKEN = 'Bearer WrR-_5offZOlPL6GxssPN5rZiVQziYsr'
+
+zone_ids = {'yourdomain1.com' : '398a6ba0a4d64bd818ec0f0f3ea7186e', 
+            'yourdomain2.net' : '507c755e637bc1dfd0c396be464f4f00', 
+            'yourdomain3.net' : 'ba6d295e048e63ee7b4c866ca6eb2ccb'}
+
+cached_ip_add = {}
+
+for record_name, zone_id in zone_ids.items():
+    cached_ip_add[record_name] = ('0.0.0.0', '00000000000000000000000000000000')
+
+CACHE_FILENAME   = os.path.abspath((os.path.join(os.path.dirname(__file__), 'IP_Address_Cache.pkl')))
+
+def saveCache():
+    """
+    stores the cached IP addresses and record IDs.  File should never be empty after first run.
+    """
+    with open(CACHE_FILENAME, 'wb') as CacheFile:
+        pickle.dump(cached_ip_add, CacheFile)
+
+def loadCache():
+    """
+    loads the cached IP addresses and record IDs.  If no cache file is present cache values created
+    by querying Cloulfare, via get_ip_record() calls.
+    """
+    global cache_updated
+
+    try: # Open NVM file if it exists otherwise use defaults
+        with open(CACHE_FILENAME, 'rb') as CacheFile:
+            tmp_cached_ip_add = pickle.load(CacheFile)
+        for key in tmp_cached_ip_add:
+            cached_ip_add[key] = tmp_cached_ip_add[key]
+    except:
+        for record_name, zone_id in zone_ids.items():
+            current_ip_record = get_ip_record(record_name, zone_id)
+            cached_ip_add[record_name] = current_ip_record
+        cache_updated = True
+
+
+def get_ip() -> str:
+    """
+    get the ip address of whoever executes the script
+    """
+    url = "http://icanhazip.com"
+    response = requests.get(url)
+    return str(response.text.strip())
+
+
+def set_ip(current_ip, zone_id, record_id, record_name):
+    """
+    sets the ip in via cloudflare api
+    """
+
+    url = (
+        "https://api.cloudflare.com/client/v4/zones/%(zone_id)s/dns_records/%(record_id)s"
+        % {"zone_id": zone_id, "record_id": record_id}
+    )
+
+    headers = {
+        "Authorization": API_TOKEN,
+        "Content-Type": "application/json",
+    }
+
+    payload = {"type": "A", "name": record_name, "content": current_ip}
+    response = requests.put(url, headers=headers, data=json.dumps(payload))
+    print(response.status_code)
+    pprint(response)
+
+
+def get_ip_record(record_name, zone_id):
+    """
+    gets the current A record IP address and record ID in via cloudflare api
+    """
+    
+
+    url = (
+        "https://api.cloudflare.com/client/v4/zones/%(zone_id)s/dns_records/"
+        % {"zone_id": zone_id}
+    )
+
+    headers = {
+        "Authorization": API_TOKEN,
+        "Content-Type": "application/json",
+    }
+
+    cloudflare_dns_respon = requests.get(url, headers=headers)
+    '''
+    if cloudflare_dns_respon.status_code == 200:
+        print("Ok")
+    else:
+        print(cloudflare_dns_respon.status_code)    
+'''
+    dns_data = json.loads(cloudflare_dns_respon.text)
+    for record in range(0, len(dns_data['result'])):
+        if dns_data['result'][record]['type'] == 'A':
+            return( (dns_data['result'][record]['content'], dns_data['result'][record]['id']) )
+
+
+def main():
+    global cache_updated
+
+    cache_updated = False
+    current_ip = get_ip()
+    #print(current_ip)
+
+    loadCache()
+    for record_name, zone_id in zone_ids.items():
+        if current_ip != cached_ip_add[record_name][0]:
+            record_id = cached_ip_add[record_name][1]
+            set_ip(current_ip, zone_id, record_id, record_name)
+            cache_updated = True
+
+    if cache_updated == True:
+        saveCache()
+
+if __name__ == "__main__":
+    main()
